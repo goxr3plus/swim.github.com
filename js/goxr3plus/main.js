9ff6f6ca0bf5dd8d8c744e528ee83940551a62be
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-07-05 16:26:44 +0300

    Auto detection of latest version

--- js/goxr3plus/main.js
+++ js/goxr3plus/main.js
@@ -1,15 +1,17 @@
-//var apiRoot = "https://api.github.com/";
-var version = "113";
-var downloadURL = "https://github.com/goxr3plus/XR3Player/releases/download/V3."+version+"/XR3Player_Installer.exe";
-// "https://github.com/goxr3plus/XR3Player/releases/download/V3."+version+"/XR3Player.Update."+version+".zip";
+function findLatestReleaseURL() {
+  //Get the version info from github
+  $.getJSON("https://api.github.com/repos/goxr3plus/XR3Player/releases/latest").done(function(json) {
+    //console.log(json);
+    var version = json.name;
+    console.log(version);
+    var downloadURL = "https://github.com/goxr3plus/XR3Player/releases/download/" + version + "/XR3Player_Installer.exe";
 
-
-function appendDownloadURLS() {
-
-    //console.log(downloadURL);
-    $("#version").html(version+" (29/06/2018)");
+    $("#version").html(version + " (29/06/2018)");
     $(".downloadXR3Player").attr("href", downloadURL);
-    $("#versionHeader").html("<strong>Installers for (XR3Player V." + version + ")</strong>");
+    $("#versionHeader").html("<strong>Installers for (XR3Player " + version + ")</strong>");
+  //  console.log("Version : " + version + " Download URL : " + downloadURL);
+  });
+
 
 }
 
